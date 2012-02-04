06d043d0a936f335447b457d78ba65677c52c62b
Martijn <martijndurnez@gmail.com>
2012-02-04 09:35:29 +0100

    removed dirt from registry

--- app/config/registry.php
+++ app/config/registry.php
@@ -1,10 +1,3 @@
 <?php
 require_once ROOT. '/vendor/php-activerecord/ActiveRecord.php';
-require_once 'vendor/Slim-Extras/TwigView.php';
-
-foreach(glob('app/functions/*.php') as $function) {
-	include $function;
-}
-foreach(glob('app/classes/*.php') as $class) {
-	include $class;
-}
+require_once ROOT .'/vendor/Slim-Extras/TwigView.php';
